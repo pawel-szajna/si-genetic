3df2b48dd3d3f9852f09e0c72350a76774a492a9
Paweł Szajna <szajnapawel@gmail.com>
2017-03-14 18:02:27 +0100

    Added evaluator functions

--- si/schedulers/genetic.cc
+++ si/schedulers/genetic.cc
@@ -1,11 +1,14 @@
+#include <functional>
 #include <map>
 #include <random>
 #include <set>
+#include <string>
 #include <vector>
 
 #include "si/resource.hh"
 #include "si/task.hh"
 #include "si/schedule.hh"
+#include "si/schedulers/greedy.hh"
 
 #include "genetic.hh"
 
@@ -14,35 +17,72 @@
 namespace si::schedulers
 {
 
-typedef std::vector<std::vector<int>> population;
+population initialize(schedule& s, std::mt19937& gen, int pop, int tasks)
+{
+	population p(pop, std::vector<int>(tasks, -1));
+	for (auto& individual : p) {
+		for (int i = 0; i < tasks; ++i) {
+			std::vector<int> valid = s.satisfying(i + 1);
+			if (valid.empty()) {
+				throw std::invalid_argument("No resource can solve task #" + std::to_string( i + 1 ));
+			}
+			std::uniform_int_distribution<> dis(0, valid.size() - 1);
+			individual[i] = valid.at(dis(gen));
+		}
+	}
+	return p;
+}
 
-//template<typename F1, typename F2>
-void optimize(schedule& s, std::vector<int>& assignments, std::vector<int>& times, int pop, int epochs /*F1 evaluate, F2 select*/)
+void print_individual(sample& v)
+{
+	std::cout << "[ ";
+	for (auto i : v) {
+		std::cout << i << " ";
+	}
+	std::cout << "]\n";
+}
+
+int time_evaluator(schedule& s, sample& individual)
+{
+	int result = 0;
+	sample times(individual.size(), 0);
+	build_timestamps(s, individual, times);
+	
+	for (unsigned i = 0; i < individual.size(); ++i) {
+		int current = s.task_at(i + 1).duration + times.at(i);
+		if (current > result) {
+			result = current;
+		}
+	}
+
+	return result;
+}
+
+int cost_evaluator(schedule& s, sample& individual)
+{
+	int result = 0;
+
+	for (unsigned i = 0; i < individual.size(); ++i) {
+		result += (int)((double)(s.task_at(i + 1).duration) * s.resource_at(individual.at(i)).salary);
+	}
+
+	return result;
+}
+
+void optimize(schedule& s, sample& assignments, sample& times, int pop, int epochs, evaluator evaluate, selector select)
 {
 	std::random_device rd;
 	std::mt19937 gen(rd());
 
 	int tasks = s.tasks.size();
 
-	population p(pop, std::vector<int>(tasks, -1));
-	for (auto& individual : p) {
-		for (int i = 0; i < tasks; ++i) {
-			std::vector<int> valid = s.satisfying(i + 1);
-			if (valid.empty()) {
-				throw std::invalid_argument("No resource can solve task #" + ( i + 1 ));
-			}
-			std::uniform_int_distribution<> dis(0, valid.size() - 1);
-			individual[i] = valid.at(dis(gen));
-		}
-	}
+	population p = initialize(s, gen, pop, tasks);
 
 	for (auto& v : p) {
-		std::cout << "[ ";
-		for (auto i : v) {
-			std::cout << i << " ";
-		}
-		std::cout << "]\n";
+		print_individual(v);
 	}
 }
+
+
 
 }
--- si/schedulers/genetic.hh
+++ si/schedulers/genetic.hh
@@ -4,8 +4,15 @@
 namespace si::schedulers
 {
 
-//template<typename F1, typename F2>
-void optimize(schedule& s, std::vector<int>& assignments, std::vector<int>& times, int pop, int epochs /*, F1 evaluate, F2 select*/);
+typedef std::vector<int> sample;
+typedef std::vector<std::vector<int>> population;
+typedef std::function<int(schedule&, sample&)> evaluator;
+typedef std::function<int(population)> selector;
+
+void optimize(schedule& s, sample& assignments, sample& times, int pop, int epochs, evaluator evaluate, selector select);
+
+int time_evaluator(schedule& s, sample& individual);
+int cost_evaluator(schedule& s, sample& individual);
 
 }
 
